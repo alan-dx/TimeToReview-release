2821c0482efb03a2bc19b7b2733f0ce95a598ed9
Alan Almeida <alanalmeida.emp@gmail.com>
2021-01-24 03:07:01 -0300

    first commit on ttr release

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>TTR agora vai, fé em Deus</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+
+export default App;
